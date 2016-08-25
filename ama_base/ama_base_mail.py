cdd288b032e2e0cb0abec561b2c71a681e6174a6
Martin Heisig <m.heisig@gmx.net>
2016-08-25 17:10:02 +0200

    fix to save first body of email

--- ama_base/ama_base_mail.py
+++ ama_base/ama_base_mail.py
@@ -3,6 +3,7 @@ from openerp import tools, SUPERUSER_ID
 import xmlrpclib
 import email
 import logging
+from openerp.addons.mail.mail_message import decode
 
 _logger = logging.getLogger(__name__)
 
@@ -157,10 +158,11 @@ class mail_thread(models.Model):
                 save_original = True
                 _logger.debug(msg_txt['From'])
         
-        '''i = 0
-        for item in msg_txt.walk():
-            _logger.debug(str(i) + ": " + str(item))
-            i+=1'''
+        '''for item in msg_txt.walk():
+            if item.has_key('Content-Transfer-Encoding') and item['Content-Transfer-Encoding'] == 'base64'
+            if item.get_content_type() == 'message/delivery-status':
+                item.set_type('message/rfc822')'''
+            
                     
         # parse the message, verify we are not in a loop by checking message_id is not duplicated
         msg = self.message_parse(cr, uid, msg_txt, save_original=save_original, context=context)
@@ -181,3 +183,74 @@ class mail_thread(models.Model):
         routes = self.message_route(cr, uid, msg_txt, msg, model, thread_id, custom_values, context=context)
         thread_id = self.message_route_process(cr, uid, msg_txt, msg, routes, context=context)
         return thread_id
+        
+    def _message_extract_payload(self, message, save_original=False):
+        """Extract body as HTML and attachments from the mail message"""
+        attachments = []
+        body = u''
+        if save_original:
+            attachments.append(('original_email.eml', message.as_string()))
+
+        # Be careful, content-type may contain tricky content like in the
+        # following example so test the MIME type with startswith()
+        #
+        # Content-Type: multipart/related;
+        #   boundary="_004_3f1e4da175f349248b8d43cdeb9866f1AMSPR06MB343eurprd06pro_";
+        #   type="text/html"
+        if not message.is_multipart() or message.get('content-type', '').startswith("text/"):
+            encoding = message.get_content_charset()
+            body = message.get_payload(decode=True)
+            body = tools.ustr(body, encoding, errors='replace')
+            if message.get_content_type() == 'text/plain':
+                # text/plain -> <pre/>
+                body = tools.append_content_to_html(u'', body, preserve=True)
+        else:
+            alternative = False
+            mixed = False
+            html = u''
+            for part in message.walk():
+                if part.get_content_type() == 'multipart/alternative':
+                    alternative = True
+                if part.get_content_type() == 'multipart/mixed':
+                    mixed = True
+                if part.get_content_maintype() == 'multipart':
+                    continue  # skip container
+                # part.get_filename returns decoded value if able to decode, coded otherwise.
+                # original get_filename is not able to decode iso-8859-1 (for instance).
+                # therefore, iso encoded attachements are not able to be decoded properly with get_filename
+                # code here partially copy the original get_filename method, but handle more encoding
+                filename=part.get_param('filename', None, 'content-disposition')
+                if not filename:
+                    filename=part.get_param('name', None)
+                if filename:
+                    if isinstance(filename, tuple):
+                        # RFC2231
+                        filename=email.utils.collapse_rfc2231_value(filename).strip()
+                    else:
+                        filename=decode(filename)
+                encoding = part.get_content_charset()  # None if attachment
+                # 1) Explicit Attachments -> attachments
+                if filename or part.get('content-disposition', '').strip().startswith('attachment'):
+                    attachments.append((filename or 'attachment', part.get_payload(decode=True)))
+                    continue
+                # 2) text/plain -> <pre/>
+                if part.get_content_type() == 'text/plain' and (not alternative or not body):
+                    body = tools.append_content_to_html(body, tools.ustr(part.get_payload(decode=True),
+                                                                         encoding, errors='replace'), preserve=True)
+                # 3) text/html -> raw
+                elif part.get_content_type() == 'text/html':
+                    # mutlipart/alternative have one text and a html part, keep only the second
+                    # mixed allows several html parts, append html content
+                    append_content = not alternative or (html and mixed)
+                    html = tools.ustr(part.get_payload(decode=True), encoding, errors='replace')
+                    if not append_content:
+                        if body:
+                            body = tools.append_content_to_html(body, html, plaintext=False)
+                        else:
+                            body = html
+                    else:
+                        body = tools.append_content_to_html(body, html, plaintext=False)
+                # 4) Anything else -> attachment
+                else:
+                    attachments.append((filename or 'attachment', part.get_payload(decode=True)))
+        return body, attachments
