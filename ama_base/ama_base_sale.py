cab82d519933497d60574e9f0ebdede6c3bf34eb
Martin Heisig <m.heisig@gmx.net>
2016-09-20 18:44:21 +0200

    added customer number to quotation and order list view

--- ama_base/ama_base_sale.py
+++ ama_base/ama_base_sale.py
@@ -58,6 +58,7 @@ class amamedis_sale_order(models.Model):
 
     delivery_date = fields.Char('Lieferdatum')
     client_order_date = fields.Date('Bestelldatum')
+    ref = fields.Char('Kundennummer', related='partner_id.ref')
 
 
 class sale_order_line(models.Model):
